e29380c9e8d004a0e565f972f2d0b165c754161e
Fabio Cardoso de Souza <52511011+souzafc@users.noreply.github.com>
2025-02-20 17:36:18 -0500

    Update main.py

--- helloworld-samples/app-python/main.py
+++ helloworld-samples/app-python/main.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
-# set up root route
+# Set up root route
 @app.route("/")
 def hello_world():
-	return "Hello World"
+    response = {
+        "message": "Hello World",
+        "status": 200
+    }
+    return jsonify(response)
 
 # Get the PORT from environment
 port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=int(port))
-
+    app.run(host='0.0.0.0', port=int(port))
