1500cb92681e80a29c2dbeb0f1658993e2d79960
Fabio Cardoso de Souza <52511011+souzafc@users.noreply.github.com>
2025-02-21 08:59:57 -0500

    Update main.py

--- helloworld-samples/app-python/main.py
+++ helloworld-samples/app-python/main.py
@@ -14,26 +14,30 @@ def process_string():
         "original": input_text,
         "modified": result
     }
+    print('acionado process')
     return jsonify(response)
 
 # Rota para obter o nome modificado
 @app.route("/get_name", methods=["GET"])
 def get_name():
-    nome = request.args.get("nome", "")  # Obtém o parâmetro 'nome' da URL
-    result = nome + "_abc"  # Adiciona "_abc" ao nome
+    name = request.args.get("name", "") 
+    result = name + "_abc" 
     
     response = {
-        "original": nome,
+        "original": name,
         "modified": result
     }
+    print('acionado get')
     return jsonify(response)
 
 # Rota padrão
 @app.route("/")
 def hello_world():
+    print('acionado raiz')
     return jsonify({"message": "Hello World", "status": 200})
 
 # Obtém a porta do ambiente
 port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
+    print('init')
     app.run(host='0.0.0.0', port=int(port))
